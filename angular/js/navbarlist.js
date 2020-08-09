3db954efb4aa2c27973d124601a24a1e9a13dd6a
programmerbikki <45654780+bikkisingh886@users.noreply.github.com>
2020-08-09 13:09:50 +0530

    Update navbarlist.js

--- angular/js/navbarlist.js
+++ angular/js/navbarlist.js
@@ -1,4 +1,4 @@
-var app = angular.module("myApp",['ngRoute']);
+var app = angular.module("myApp",['ngRoute', 'ui.bootstrap']);
 	app.controller("myctrl",function($scope){
            $scope.navlist = [
                {name:'Home',link:'#!home'},
